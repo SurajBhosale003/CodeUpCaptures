3db645908f54c92ce591124897f1f535c62740b7
Suraj Ranajit Bhosale <surajranajitbhosale003@gmail.com>
2023-07-30 20:47:02 +0530

    react work adding navbar

--- client/src/scenes/navbar/Navbar.jsx
+++ client/src/scenes/navbar/Navbar.jsx
@@ -1,9 +1,21 @@
+import { useState } from "react"
+import { 
+  Box,
+  IconButton,
+  InputBase,
+  Typography,
+  Select,
+  MenuItem,
+  FormControl,
+  useTheme,
+  useMediaQuery
+} from "@mui/material";
 
 
 function Navbar() {
   return (
     <>
-    <div>Navbar</div>
+      <div>Navbar</div>
     </>
   )
 }
